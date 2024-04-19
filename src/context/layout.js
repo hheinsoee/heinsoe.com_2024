6c65975382cd56d290244e0dca2725d351ee7f26
Hein Soe <35089574+hheinsoee@users.noreply.github.com>
2024-04-19 23:52:37 +0630

    layout

--- src/context/layout.js
+++ src/context/layout.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useMemo, useState } from "react";
 
 import { useTheme } from "@/context/theme";
 import Link from "next/link";
@@ -8,18 +8,40 @@ import { Button } from "antd";
 
 const LayoutContext = createContext(); // Rename the context variable
 export const LayoutProvider = ({ children }) => {
-  const [menuSize, setMenuSize] = useState("l"); //l,m,s, null
+  const [menuSize, setMenuSize] = useState("m"); //l,m,s, null
   const { setDarkMode, isDark } = useTheme();
+
   return (
     <LayoutContext.Provider value={{ menuSize, setMenuSize }}>
-      <nav>
-        {menuSize}
-        <Button type="primary" onClick={() => setDarkMode(!isDark)}>
-          Primary Button
-        </Button>
-        <Link href={myLink.blog()}>Blog</Link>
-      </nav>
-      {children}
+      <div
+        className={`layout ${menuSize}`}
+        style={{
+          display: "flex",
+          margin: "auto",
+        }}
+      >
+        <div
+          className="menu"
+          style={{
+            flex: 1,
+          }}
+        >
+          <nav className="sticky h-screen" style={{ top: 0 }}>
+            <Button type="primary" onClick={() => setDarkMode(!isDark)}>
+              Primary Button
+            </Button>
+            <Link href={myLink.blog()}>Blog</Link>
+          </nav>
+        </div>
+        <div className="flex-1">
+          {["l", "m", "s", "no"].map((s) => (
+            <Button onClick={() => setMenuSize(s)} key={s}>
+              {s}
+            </Button>
+          ))}
+          {children}
+        </div>
+      </div>
     </LayoutContext.Provider>
   );
 };
